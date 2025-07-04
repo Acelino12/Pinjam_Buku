82088af4428850b55ea82c6d805d43f620fd5661
Marselino <mateusmarselino.k@gmail.com>
2025-07-04 15:48:00 +0700

    create author controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Authors;
+use App\Models\Countries;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function index(){
+        $dataAuthors = Authors::all();
+
+        return view('authors.index', ['dataAuthors' => $dataAuthors]);
+    }
+
+    public function showinput(){
+        $country = Countries::all();
+        return view('authors.add-author',['country' => $country]);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|max:255',
+            'bio' => 'required',
+            'date_of_birth' => 'required',
+            'country_id' => 'required',
+            'web_url' => 'required',
+        ]);
+
+        Authors::create([
+            'name' => $request->name,
+            'bio' => $request->bio,
+            'date_of_birth' => $request->date_of_birth,
+            'country_id' => $request->country_id,
+            'web_url' => $request->web_url
+        ]);
+
+        return redirect('/authors')->with(['success','berhasil menambah author']);
+    }
+}
