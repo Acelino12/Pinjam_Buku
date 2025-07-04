2efc45a435bf04451b4d3d225d609aaa9dc9d416
Marselino <mateusmarselino.k@gmail.com>
2025-07-04 15:40:42 +0700

    create country

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Countries;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    public function index(){
+        $country = Countries::all();
+
+        return view('feature.country',['country' => $country]);
+    }
+    public function create(Request $request){
+        $request->validate([
+            'name' => 'required|max:100'
+        ]);
+
+        Countries::create([
+            'name' => $request->name,
+        ]);
+    }
+}
