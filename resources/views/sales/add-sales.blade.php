3bfaa0e9cd668e38edc5f5e62fb7cae1a353d16f
Marselino <mateusmarselino.k@gmail.com>
2025-07-07 10:39:46 +0700

    create sales

---
+++ resources/views/sales/add-sales.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.crud')
+
+@section('title', 'new sales')
+
+@section('css')
+
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('js')
+
+@endsection
---
+++ resources/views/sales/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('title', 'Sales')
+
+@section('content')
+    <h3>Sales</h3>
+    <br>
+    <a href="/newsales" class="btn btn-primary">tambah data</a>
+    <br>
+    @if (Session::has('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{Session::get('success')}}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Date</th>
+                <th>Status</th>
+                <th>Payment Status</th>
+                <th>Button</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($datasales as $item)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->purchase_date}}</td>
+                    <td>{{$item->status}}</td>
+                    <td>{{$item->payment_status}}</td>
+                    <td>
+                        <a href="saledetail/{{$item->id}}">Detail</a>
+                        <a href="/{{$item->id}}">Jadwal</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
