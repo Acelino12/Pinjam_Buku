0c9d9e769383ceed7f7a3b236c0607a850627a1e
Marselino <mateusmarselino.k@gmail.com>
2025-07-08 11:41:18 +0700

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -5,60 +5,87 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\RentalController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\UsersController;
-use App\Models\Bookstore_users;
 use Illuminate\Support\Facades\Route;
 
+
+// dashbord
+Route::get('/home', function () {
+    return view('layouts.app');
+})->middleware('auth');
+
+// auth
 Route::get('/', function () {
     return view('auth.login');
 });
-
-Route::get('/login',function(){
+Route::get('/login', function () {
     return view('auth.login');
+})->name('login');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/register', 'formRegister');
+    Route::post('/postauth', 'register');
+    Route::get('/auth', 'login');
+    Route::get('/logout', 'logout');
 });
 
-Route::get('/register',[AuthController::class,'formRegister']);
-Route::post('/postauth',[AuthController::class,'register']);
-Route::get('/auth',[AuthController::class,'login']);
-Route::get('/home', function(){
-    return view('layouts.app');
+
+Route::middleware('auth')->group(function () {
+
+    // Country
+    Route::prefix('countrys')->controller(CountryController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('newcountry', 'input');
+        Route::post('addcountry', 'create');
+        Route::get('delete/{id}', 'delete');
+    });
+
+    // input users
+    Route::prefix('users')->controller(UsersController::class)->group(function () {
+        Route::get('/', 'index'); //all users
+        Route::get('/newuser', 'showinput'); // menampilkan daftar negara
+        Route::post('/adduser', 'store');
+        Route::get('/userdetails/{id}', 'show');
+        Route::get('/userdelete/{id}', 'delete');
+        Route::get('/softdelete', 'softdelete');
+    });
+
+    // publisher
+    Route::prefix('publishers')->controller(PublisherController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('newpublisher', 'showinput');
+        Route::post('addpublisher', 'store');
+    });
+
+    // Author
+    Route::prefix('authors')->controller(AuthorController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/newauthor', 'showinput');
+        Route::post('/addauthor', 'store');
+    });
+
+    // Book
+    Route::prefix('/books')->controller(BooksController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/newbook', 'showinput');
+        Route::post('/addbook', 'store');
+        Route::get('/bookdetail/{id}', 'show');
+    });
+
+    // Rental
+    Route::prefix('/rental')->controller(RentalController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/newrental', 'showinput');
+    });
+
+    // Sales
+    Route::prefix('sales')->controller(SalesController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/newsales', function () {
+            return view('sales.add-sales');
+        });
+        Route::post('/addsales','store');
+    });
 });
-
-Route::get('/countrys',[CountryController::class,'index']);
-Route::get('/newcountry',function(){
-    return view('feature.add-country');
-});
-Route::post('/addcountry',[CountryController::class,'create']);
-
-
-Route::get('/sales',[SalesController::class,'index']);
-Route::get('/newsales',function(){
-    return view('sales.add-sales');
-});
-Route::post('/addsales',[SalesController::class,'']);
-
-
-// input users
-Route::get('/users',[UsersController::class,'index']); // all users
-Route::get('/newuser',[UsersController::class,'showinput']); // untuk menampilka daftar negara
-Route::post('/adduser',[UsersController::class,'store']); // input new user
-Route::get('/userdetails/{id}',[UsersController::class,'show']); // detail user
-Route::get('/userdelete/{id}',[UsersController::class,'delete']); // delete user
-Route::get('/softdelete',[UsersController::class,'softdelete']);
-
-// publisher
-Route::get('/publisher', [PublisherController::class,'index']);
-Route::get('/newpublisher',[PublisherController::class,'showinput']);
-Route::post('/addpublisher',[PublisherController::class,'store']);
-
-// Author
-Route::get('/authors',[AuthorController::class,'index']);
-Route::get('/newauthor',[AuthorController::class,'showinput']);
-Route::post('/addauthor',[AuthorController::class,'store']);
-
-// Book
-Route::get('/books',[BooksController::class,'index']);
-Route::get('/newbook',[BooksController::class,'showinput']);
-Route::post('/addbook',[BooksController::class,'store']);
-Route::get('/bookdetail/{id}',[BooksController::class,'show']);
