310cca1ecb61a8a94b0bec090dee00af088f060e
Marselino <mateusmarselino.k@gmail.com>
2025-07-09 20:05:05 +0700

    create error 503 maintenence

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>503 - maintenance</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            text-align: center;
+            margin-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>503</h1>
+    <h3>Aplikasi dalam pemliharaan</h3>
+    <h3>Tunggu beberapa saat lagi</h3>
+</body>
+</html>
